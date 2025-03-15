19327deab33f271614df24d8de5016afd1dac2f7
박은비 <ebpark9511@gmail.com>
2025-03-15 23:11:55 +0900

    feat: MusicDomain에 권한 UseCase추가

---
+++ Projects/Domain/MusicDomain/Interface/UseCase/AuthorizeMusicLibraryUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  AuthorizeMusicLibraryUseCase.swift
+//  MusicDomain
+//
+//  Created by 박은비 on 3/15/25.
+//  Copyright © 2025 ebpark. All rights reserved.
+//
+
+import Foundation
+
+protocol AuthorizeMusicLibraryUseCase {
+    func execute() async throws
+}
